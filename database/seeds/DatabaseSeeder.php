572e587080cdbaf39ae14fbe132aaadea8c4a435
Abdul Majid <abdulmajidcse@gmail.com>
2022-01-06 10:56:38 +0600

    composer updated and db seed created for new super admin.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        // create admin
+        User::create([
+            'name'     => 'Super Admin',
+            'email'    => 'super_admin@gmail.com',
+            'password' => Hash::make(12345678),
+        ]);
     }
 }
